a0376f374ad83cef982a88fda42db5616c4d4ff6
Jerry Fu <fusun@ms7.hinet.net>
2016-04-11 16:03:27 +0800

    Add Project

---
+++ Work.WebProj/Controllers/NeighborController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using DotWeb.Controller;
+using ProcCore.Business.DB0;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotWeb.Controllers
+{
+    public class NeighborController : WebUserController
+    {
+        public ActionResult Index()
+        {
+            return View("Neighbor");
+        }
+        public ActionResult List()
+        {
+            return View("Neighbor_list");
+        }
+        public ActionResult Content()
+        {
+            return View("Neighbor_content");
+        }
+    }
+
+}
