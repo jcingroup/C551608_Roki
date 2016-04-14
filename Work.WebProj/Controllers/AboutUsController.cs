3568cc113014186e18229865ee0b8875e11d446f
TinWu <yyuuix@gmail.com>
2016-04-14 17:54:58 +0800

    首頁切版ok

--- Work.WebProj/Controllers/AboutUsController.cs
+++ Work.WebProj/Controllers/AboutUsController.cs
@@ -9,15 +9,7 @@ namespace DotWeb.Controllers
     public class AboutUsController : WebUserController
     {
         public ActionResult Index()
-        {
-            Edit item = new Edit();
-            using (var db0 = getDB0())
-            {
-                #region get content
-                item = db0.Edit.Find((int)EditorState.Aboutus);
-                #endregion
-            }
-            ViewBag.content = item.edit_content;
+        {            
             return View("AboutUs");
         }
     }
